e96019bd51f47989dcc3edbebc9cc99e654272a4
Aswin <aswinjikz071@gmail.com>
2023-12-22 16:56:19 +0530

    property CRUD added

--- realestate/property/urls.py
+++ realestate/property/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('properties/', views.property_list, name='property_list'),
+    path('properties/add/', views.add_property, name='add_property'),
+    path('properties/<int:pk>/edit/', views.edit_property, name='edit_property'),
+    path('properties/<int:pk>/delete/', views.delete_property, name='delete_property'),
     
 ]
--- realestate/property/views.py
+++ realestate/property/views.py
@@ -1,3 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Property
+from .forms import PropertyForm
 
-# Create your views here.
+def property_list(request):
+    properties = Property.objects.all()
+    return render(request, 'property_list.html', {'properties': properties})
+
+def add_property(request):
+    if request.method == 'POST':
+        form = PropertyForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('property_list')
+    else:
+        form = PropertyForm()
+    return render(request, 'add_property.html', {'form': form})
+
+def edit_property(request, pk):
+    property = get_object_or_404(Property, pk=pk)
+    if request.method == 'POST':
+        form = PropertyForm(request.POST, instance=property)
+        if form.is_valid():
+            form.save()
+            return redirect('property_list')
+    else:
+        form = PropertyForm(instance=property)
+    return render(request, 'edit_property.html', {'form': form})
+
+def delete_property(request, pk):
+    property = get_object_or_404(Property, pk=pk)
+    if request.method == 'POST':
+        property.delete()
+        return redirect('property_list')
+    return render(request, 'delete_property.html', {'property': property})
